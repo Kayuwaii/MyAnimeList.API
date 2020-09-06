2a156bc168f00014aa67c31dcf9e78caa45ad9ad
ALEX CORONAS ALTEMIR <alexcoralt@msn.com>
2020-09-06 18:56:50 +0200

    NOT MUCH

--- MyAnimeList.API/obj/Release/netstandard2.1/MyAnimeList.API.AssemblyInfo.cs
+++ MyAnimeList.API/obj/Release/netstandard2.1/MyAnimeList.API.AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Àlex Coronas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A simple .NET Standard wrapper for the new v2 API of MyAnimeList")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.1")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.2.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAnimeList.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAnimeList.API")]
-[assembly: System.Reflection.AssemblyVersionAttribute("0.1.1.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.1.2.0")]
 [assembly: System.Resources.NeutralResourcesLanguageAttribute("en")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
